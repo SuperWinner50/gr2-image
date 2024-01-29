7d2aff607e36e1188dbdc6a8285e6ab286c81b5b
Aaron <42575013+SuperWinner50@users.noreply.github.com>
2024-01-28 21:59:14 -0500

    More features

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,9 @@ use std::collections::HashMap;
 use std::io::Write;
 
 use silv::{RadarFile, Sweep, Ray};
-use rand_distr::StandardNormal;
-use rand::{thread_rng, Rng};
+use rayon::prelude::*;
+
+use palette::Lab;
 
 fn write_simple_colormap(data: Vec<f32>) -> Vec<u8> {
     let mut writer = std::fs::File::create("colormap.pal").unwrap();
@@ -33,28 +34,93 @@ fn write_simple_colormap(data: Vec<f32>) -> Vec<u8> {
         .collect()
 }
 
-fn write_kmeans_colormap(data: Vec<f32>) -> Vec<u8> {
-    use palette::{cast::from_component_slice, color_difference::{Ciede2000, EuclideanDistance}};
-    use palette::{FromColor, IntoColor, Lab, Srgb};
-    use kmeans_colors::{get_kmeans_hamerly, Calculate, Kmeans, MapColor, Sort};
-    use rayon::prelude::*;
+fn total_cost(colors: &Vec<(usize, Lab)>) -> f32 {
+    use palette::color_difference::Ciede2000;    
 
-    const RUNS: usize = 100;
-    const K: usize = 50;
+    colors.windows(2).map(|c| c[0].1.difference(c[1].1).powi(2)).sum::<f32>()
+}
+
+fn cost_diff(colors: &Vec<(usize, Lab)>, a: usize, b: usize) -> f32 {
+    use palette::color_difference::Ciede2000;
+
+    let mut end = 0.0;
+    if a > 0 {
+        end += colors[a - 1].1.difference(colors[b].1).powi(2) - colors[a - 1].1.difference(colors[a].1).powi(2);
+    }
+
+    if a < colors.len() - 1 {
+        end += colors[b].1.difference(colors[a + 1].1).powi(2) - colors[a].1.difference(colors[a + 1].1).powi(2);
+    }
+
+    if b > 0 {
+        end += colors[b - 1].1.difference(colors[a].1).powi(2) - colors[b - 1].1.difference(colors[b].1).powi(2);
+    }
+
+    if b < colors.len() - 1 {
+        end += colors[a].1.difference(colors[b + 1].1).powi(2) - colors[b].1.difference(colors[b + 1].1).powi(2);
+    }
+
+    end
+}
+
+// fn cost_change(colors: &Vec<(usize, Lab)>, a: usize, b: usize) -> f32 {
+//     cost_diff(colors, b, a) - cost_diff(colors, a, b)
+// }
+
+fn anneal(colors: &mut Vec<(usize, Lab)>) -> f32 {
+    use rand::Rng;
+
+    let runs = 50000;
+
+    // let max = 150.0 * K as f32;
+    // let a = 15.0;
+
+    let mut max = 10000.0;
+    let a = 15.0;
+
+    let mut rng = rand::thread_rng();
+
+    for i in 0..runs {
+        let p1 = rng.gen_range(0..colors.len());
+        let p2 = rng.gen_range(0..colors.len());
+
+        if p1 == p2 {
+            continue;
+        }
+
+        // let temp = max * 0.5f32.powf(a * (i + 1) as f32 / runs as f32) - max * 0.5f32.powf(a) - 2.0 * K as f32;
+        let temp = max * 0.5f32.powf(a * (i + 1) as f32 / runs as f32) - max * 0.5f32.powf(a);
+
+        let change = cost_diff(colors, p1, p2);
+        let p = (-change / temp).exp();
+
+        if change <= 0.0 || p >= rand::random::<f32>() {
+            colors.swap(p1, p2);
+        }
+    }
+
+    let c = total_cost(&colors);
+    println!("Cost {c}");
+    c
+}
+
+fn write_kmeans_colormap(data: Vec<f32>) -> Vec<u8> {
+    use palette::{cast::from_component_slice, color_difference::Ciede2000};
+    use palette::{FromColor, IntoColor, Lab, Srgb};
+    use kmeans_colors::{get_kmeans_hamerly};
 
     let lab: Vec<Lab> = from_component_slice::<Srgb<f32>>(&data)
         .iter()
         .map(|x| x.into_format().into_color())
         .collect();
 
-    // let mut seed = rand::random::<u32>() as u64;
-    let mut seed = 0;
-    let mut result = (0..RUNS).into_par_iter()
+    let seed = rand::random::<u32>() as u64;
+    let result = (0..RUNS).into_par_iter()
         .map(|i| {
             get_kmeans_hamerly(
                 K,
-                120,
-                0.01,
+                100,
+                2.0,
                 true,
                 &lab,
                 seed + i as u64,
@@ -66,20 +132,17 @@ fn write_kmeans_colormap(data: Vec<f32>) -> Vec<u8> {
     println!("Min score: {}", result.score);
 
     let mut res = result.centroids.clone().into_iter().enumerate().collect::<Vec<_>>();
-    let mut sorted = vec![res.pop().unwrap()];
 
-    while res.len() > 0 {
-        let top = sorted.last().unwrap().1;
-        let next = res.iter().enumerate().fold((0, f32::MAX), |acc, (i1, (_, r))| {
-            let d = top.difference(*r);
-            if d < acc.1 {
-                (i1, d)
-            } else {
-                acc
-            }
-        });
-        sorted.push(res.remove(next.0));
-    }
+    let (sorted, cost) = (0..100).into_par_iter()
+        .map(|_| {
+            let mut res = res.clone();
+            let cost = anneal(&mut res);
+            (res, cost)
+        })
+        .reduce_with(|a, b| if a.1 < b.1 { a } else { b })
+        .unwrap();
+
+    println!("Coloring cost: {}", cost / K as f32);
 
     let idxs = sorted.iter().map(|(i, _)| *i).collect::<Vec<_>>();
 
@@ -120,7 +183,89 @@ enum Fit {
     Val(f32), 
 }
 
-fn write_image(image: impl AsRef<std::path::Path>, (x, y): (f32, f32), (rays, gates): (u32, u32), width: f32, fit: Fit, random: bool, kmeans: bool) -> Result<(), Box<dyn std::error::Error>> {
+fn weight(image: &image::Rgb32FImage, points: Vec<(f32, f32)>) -> Option<[f32; 3]> {
+    use geo::prelude::*;
+    use geo::geometry::{LineString, Polygon};
+    use geo_rasterize::{LabelBuilder, MergeAlgorithm};
+
+    let minx = points.iter().fold(f32::INFINITY, |a, &b| a.min(b.0)).floor() as usize;
+    let miny = points.iter().fold(f32::INFINITY, |a, &b| a.min(b.1)).floor() as usize;
+    let maxx = points.iter().fold(f32::NEG_INFINITY, |a, &b| a.max(b.0)).ceil() as usize;
+    let maxy = points.iter().fold(f32::NEG_INFINITY, |a, &b| a.max(b.1)).ceil() as usize;
+
+    let poly = &Polygon::new(LineString::from(points.into_iter().map(|p| (p.0 - minx as f32, p.1 - miny as f32)).collect::<Vec<_>>()), vec![]);
+
+    (0..maxx-minx).into_par_iter()
+        .map(move |xi| {
+            (0..maxy-miny).into_par_iter().map(move |yi| {
+                let (x, y) = (xi as f32, yi as f32);
+                let px = Polygon::new(LineString::from(vec![(x, y), (x + 1.0, y), (x + 1.0, y + 1.0), (x, y + 1.0)]), vec![]);
+                let weight = px.intersection(poly).signed_area();
+                if weight > 0.0 {
+                    image.get_pixel_checked((xi + minx) as u32, (yi + miny) as u32).map(|v| (weight, v.0.map(|c| c * weight)))
+                } else {
+                    None
+                }
+            })
+            .flatten()
+        })
+        .flatten()
+        .reduce_with(|a, b| (a.0 + b.0, [a.1[0] + b.1[0], a.1[1] + b.1[1], a.1[2] + b.1[2]]))
+        .map(|(total, v)| v.map(|x| x / total))
+}
+
+fn get_colors_avg(image: &image::Rgb32FImage, rays: u32, gates: u32, x: f32, y: f32, size: f32) -> HashMap<usize, [f32; 3]> {
+    let mut colors = HashMap::new();
+
+    for ray in 0..rays {
+        let a1 = 2.0 * std::f32::consts::PI * (ray as f32 - 0.5) / rays as f32;
+        let a2 = 2.0 * std::f32::consts::PI * (ray as f32 + 0.5) / rays as f32;
+
+        let (sa1, ca1) = a1.sin_cos();
+        let (sa2, ca2) = a2.sin_cos();
+
+        for gate in 0..gates {
+            let r1 = size * gate as f32 / gates as f32;
+            let r2 = size * (gate + 1) as f32 / gates as f32;
+
+            let pts = [(r1 * ca1, r1 * sa1), (r2 * ca1, r2 * sa1), (r2 * ca2, r2 * sa2), (r1 * ca2, r1 * sa2)].map(|p| (p.0 + x * image.width() as f32, p.1 + y * image.height() as f32));
+
+            if pts.iter().all(|p| 0.0 > p.0 || p.0 > image.width() as f32) || pts.iter().all(|p| 0.0 > p.1 || p.1 > image.height() as f32) {
+                continue;
+            }
+
+            let idx = gate as usize + ray as usize * gates as usize;
+
+            if let Some(color) = weight(image, pts.into()) {
+                colors.insert(idx, color);
+            }
+        }
+    }
+
+    colors
+}
+
+fn get_colors_nearest(image: &image::Rgb32FImage, rays: u32, gates: u32, x: f32, y: f32, size: f32) -> HashMap<usize, [f32; 3]> {
+    let mut colors = HashMap::new();
+
+    for ray in 0..rays {
+        let azimuth = 360.0 * ray as f32 / rays as f32;
+
+        for gate in 0..gates {
+            let x = (azimuth.to_radians().cos() * gate as f32 * (size / gates as f32) + x * image.width() as f32).round() as u32;
+            let y = (azimuth.to_radians().sin() * gate as f32 * (size / gates as f32) + y * image.height() as f32).round() as u32;
+
+            if 0 < x && x < image.width() && 0 < y && y < image.height() {
+                let idx = gate as usize + ray as usize * gates as usize;
+                colors.insert(idx, image[(x, y)].0);
+            }
+        }
+    }
+
+    colors
+}
+
+fn write_image(image: impl AsRef<std::path::Path>, (x, y): (f32, f32), (rays, gates): (u32, u32), width: f32, fit: Fit, kmeans: bool, antialias: bool) -> Result<(), Box<dyn std::error::Error>> {
     let image = image::open(image)?.to_rgb32f();
     
     let size = match fit {
@@ -129,32 +274,11 @@ fn write_image(image: impl AsRef<std::path::Path>, (x, y): (f32, f32), (rays, ga
         Fit::Val(x) => x
     };
 
-    let mut colors = std::collections::HashMap::new();
-
-    use geo::geometry::{Rect, Polygon};
-
-    for xi in 0..image.width() {
-        for yi in 0..image.height() {
-            let (x, y) = (xi as f32 - x * image.width() as f32, yi as f32 - y * image.height() as f32);
-            let r = x.hypot(y);
-            let a = y.atan2(x).rem_euclid(std::f32::consts::PI * 2.0);
-
-            let ri = (r / (size / gates as f32)) as usize;
-
-            if ri >= gates as usize {
-                continue;
-            }
-
-            let ai = (a / (std::f32::consts::PI * 2.0 / rays as f32)) as usize;
-            let idx = ri + ai * gates as usize;
-            colors.entry(idx).or_insert(Vec::new()).push(image[(xi, yi)].0);
-        }
-    }
-
-    let colors = colors.into_iter().map(|(k, v)| {
-        let len = v.len();
-        (k, v.into_iter().reduce(|acc, v| [acc[0] + v[0], acc[1] + v[1], acc[2] + v[2]]).unwrap().map(|v| v / len as f32))
-    }).collect::<HashMap<_, _>>();
+    let colors = if antialias {
+        get_colors_avg(&image, rays, gates, x, y, size)
+    } else {
+        get_colors_nearest(&image, rays, gates, x, y, size)
+    };
 
     let data: Vec<_> = colors.values().cloned().flatten().collect();
 
@@ -194,9 +318,6 @@ fn write_image(image: impl AsRef<std::path::Path>, (x, y): (f32, f32), (rays, ga
         };
 
         for gate in 0..gates {
-            let x = (azimuth.to_radians().cos() * gate as f32 * (size / gates as f32) + x * image.width() as f32).round() as u32;
-            let y = (azimuth.to_radians().sin() * gate as f32 * (size / gates as f32) + y * image.height() as f32).round() as u32;
-
             let color_idx = if let Some(o) = colors.get(&((gate + rayi * gates) as usize)) {
                 *o as f64 / 2.0 - 32.0
             } else {
@@ -214,6 +335,9 @@ fn write_image(image: impl AsRef<std::path::Path>, (x, y): (f32, f32), (rays, ga
 
     Ok(())
 }
+
+const RUNS: usize = 30;
+const K: usize = 50;
 
 fn parse_args() {
     use clap::{Command, Arg};
@@ -247,17 +371,16 @@ fn parse_args() {
                 .long("width")
                 .help("Gate width")
                 .default_value("100"),
-            Arg::new("random")
-                .short('R')
-                .long("random")
-                .action(clap::ArgAction::SetTrue)
-                .help("Add gaussian randomness for non-kmeans runs")
-                .conflicts_with("kmeans"),
             Arg::new("kmeans")
                 .short('k')
-                .long("use_kmeans")
+                .long("kmeans")
                 .action(clap::ArgAction::SetTrue)
-                .help("Uses a kmeans algorithm to make a custom colormap based off of the input image"),
+                .help("Use a kmeans algorithm to make a custom colormap based off of the input image. Multithreaded."),
+            Arg::new("antialias")
+                .short('a')
+                .long("aa")
+                .action(clap::ArgAction::SetTrue)
+                .help("Average colors inside of each cell. May add a significant amount of processing time. Multithreaded."),
             Arg::new("files")
                 .required(true)
         ]).get_matches();
@@ -274,7 +397,7 @@ fn parse_args() {
     let width = m.get_one::<String>("width").unwrap().parse().unwrap();
     let im = m.get_one::<String>("files").unwrap();
 
-    match write_image(&im, (pos[0], pos[1]), (res[0], res[1]), width, fit, m.get_flag("random"), m.get_flag("kmeans")) {
+    match write_image(&im, (pos[0], pos[1]), (res[0], res[1]), width, fit, m.get_flag("kmeans"), m.get_flag("antialias")) {
         Err(e) => println!("Error {e} occured while reading {im:?}"),
         _ => ()
     }
